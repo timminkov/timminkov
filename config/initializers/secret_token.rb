a3210f66cd66c9d77a3dfc5699d58517cb4e7c34
Tim Minkov <timothyminkov@gmail.com>
2014-05-13 16:35:54 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timminkov::Application.config.secret_key_base = 'df14e8d443e3decbf0f311646f614fa8ce2b22e26deed46666a435ab26a97d59f94ebc4a3aab9766682c2b2f1f9fd933e976cd5394f5068d031af96bb701b2c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Timminkov::Application.config.session_store :cookie_store, key: '_timminkov_session'
