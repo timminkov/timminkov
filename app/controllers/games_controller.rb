1a2d811b401c922e95b08cff714f78db2a815596
Tim Minkov <timothyminkov@gmail.com>
2015-02-01 02:27:24 -0800

    Add immortal legion endpoint

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,8 @@ class GamesController < ApplicationController
     @images = Dir.glob("public/images/maggie/*.gif")
   end
 
+  def immortal_legion
+    render layout: 'immortal_legion'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get '/space_garbage_warrior_2988', to: 'games#space_garbage_warrior', as: 'space_garbage_warrior'
     get '/homebreaker', to: 'games#homebreaker', as: 'homebreaker'
     get '/maggie', to: 'games#maggie', as: 'maggie'
+    get '/immortal_legion', to: 'games#immortal_legion', as: 'immortal_legion'
   end
 end
